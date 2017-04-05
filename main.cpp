a2791f922d467b25b8cd9f2b3fcc2f16cfd386db
JeffSandov6 <jeffry_sandoval24@yahoo.com>
2017-04-05 17:22:49 -0500

    Skeleton for main
    #include <iostream>
    #include <fstream>
    #include <sstream>
    using std::stringstream;
    
    
    
    using namespace std;
    
    
    int main()
    {
    
    	ifstream inStream;
    	ofstream outStream;
    
    
    	inStream.open("names.txt");
    
    	outStream.open("sortednames.txt");
    
    
    	string one;
    
    	inStream >> one;
    
    	outStream << one;
    
    
    
    	
    
    	cout << "the sum total of all the name score is: " << one << endl;
    
    
    	cin >> one;
    
    	return 0;
    }

--- main.cpp
+++ main.cpp
@@ -21,3 +21,42 @@ totalscore += nameScore;
 	cout << "the sum total of all the name score is: " << endl;
   
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using std::stringstream;
+
+
+
+using namespace std;
+
+
+int main()
+{
+
+	ifstream inStream;
+	ofstream outStream;
+
+
+	inStream.open("names.txt");
+
+	outStream.open("sortednames.txt");
+
+
+	string one;
+
+	inStream >> one;
+
+	outStream << one;
+
+
+
+	
+
+	cout << "the sum total of all the name score is: " << one << endl;
+
+
+	cin >> one;
+
+	return 0;
+}
